fc62be20d53ef36570e5b0df2517a72a6e02e062
Ratana <createbysenpai@gmail.com>
2024-04-23 14:00:11 +0200

    💄 Landing-screen > Add Logo

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="p-5 flex justify-between items-cente border shadow-effect">
+      <Image src={"./logo.svg"} width={160} height={100} alt="logo" />
+      <Button>Get Started</Button>
+    </div>
+  );
 }
 
 export default Header;
