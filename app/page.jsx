b58684fb824da1d73729f2415b10805f071510fa
Ratana <createbysenpai@gmail.com>
2024-04-23 13:38:22 +0200

    ✨ Landing-screen > Create Dasboard and Header

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h2>Subscribe Now</h2>
-      <Button>Subscribe</Button>
+      <Header />
     </div>
   );
 }
