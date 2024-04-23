86aa445dcc5114e324ac74805f97264ef74391e5
Ratana <createbysenpai@gmail.com>
2024-04-23 15:52:25 +0200

    ✨ Authentication > Install Clerk

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
@@ -10,8 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="fr">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
