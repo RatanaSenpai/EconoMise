8fe3e612b1ca1f9c8eac2a9c9cbd37c8cec875b4
Ratana <createbysenpai@gmail.com>
2024-04-24 13:34:27 +0200

    🚧 Side-navbar > Add menu list and styling

--- app/(routes)/_components/SideNav.jsx
+++ app/(routes)/_components/SideNav.jsx
@@ -1,10 +1,50 @@
+import { UserButton } from "@clerk/nextjs";
+import { LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 function SideNav() {
+  const menuList = [
+    {
+      id: 1,
+      name: "Dashboard",
+      icon: LayoutGrid,
+    },
+    {
+      id: 2,
+      name: "Budget",
+      icon: PiggyBank,
+    },
+    {
+      id: 3,
+      name: "Expenses",
+      icon: ReceiptText,
+    },
+    {
+      id: 4,
+      name: "Upgrade",
+      icon: ShieldCheck,
+    },
+  ];
+
   return (
     <div className="h-screen p-5 border shadow-sm">
       <Image src={"/logo.svg"} alt="logo" width={160} height={100} />
+      <div className="mt-5">
+        {menuList.map((menu, index) => (
+          <h2
+            className="flex gap-2 items-center text-gray-500 font-medium p-5 cursor-pointer rounded-md
+          hover:text-primary hover:bg-purple-100"
+          >
+            <menu.icon />
+            {menu.name}
+          </h2>
+        ))}
+      </div>
+      <div className="fixed bottom-10 p-5 flex gap-2 ite">
+        <UserButton />
+        Profile
+      </div>
     </div>
   );
 }
