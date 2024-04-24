e24caa91d3e21c277251aa0dcd1f1eb1b779512f
Ratana <createbysenpai@gmail.com>
2024-04-24 10:05:37 +0200

    🚧 Authentication > Add user rules from Header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,15 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 function Header() {
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-5 flex justify-between items-cente border shadow-effect">
       <Image src={"./logo.svg"} width={160} height={100} alt="logo" />
-      <Button>Get Started</Button>
+      {isSignedIn ? <UserButton /> : <Button>Get Started</Button>}
     </div>
   );
 }
