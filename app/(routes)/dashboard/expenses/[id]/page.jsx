e9dedbcc4ffbd104bc5acbea92186451374b2a0e
Ratana <createbysenpai@gmail.com>
2024-04-26 13:57:00 +0200

    🚧 New-expense > Create page.jsx

---
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+import { db } from "@/utils/dbConfig";
+import { Budgets, Expenses } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq, getTableColumns, sql } from "drizzle-orm";
+import React, { useEffect } from "react";
+
+function ExpensesScreen({ params }) {
+  const { user } = useUser();
+  useEffect(() => {
+    user && getBudgetInfo();
+  }, [user]);
+
+  const getBudgetInfo = async () => {
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
+        totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+        totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+      })
+      .from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .where(eq(Budgets.id, params.id))
+      .groupBy(Budgets.id);
+  };
+
+  return (
+    <div className="p-5">
+      <h2 className="text-2xl font-bold">My Expenses</h2>
+    </div>
+  );
+}
+
+export default ExpensesScreen;
