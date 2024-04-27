d69f8f0238d5e3816adeec886da5be505a5e2e64
Ratana <createbysenpai@gmail.com>
2024-04-27 14:47:00 +0200

    🚧 Manage-expense > Configuring date of latest expense

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -11,10 +11,10 @@ function ExpenseListTable({ expensesList }) {
         <h2 className="font-bold">Action</h2>
       </div>
       {expensesList.map((expenses, index) => (
-        <div className="grid grid-cols-4 bg-slate-50 p-2">
+        <div key={expenses.id} className="grid grid-cols-4 bg-slate-50 p-2">
           <h2>{expenses.name} </h2>
           <h2>{expenses.amount} </h2>
-          <h2>{expenses.createAt} </h2>
+          <h2>{expenses.createdAt} </h2>
           <h2>
             <Trash className="text-red-600" />
           </h2>
