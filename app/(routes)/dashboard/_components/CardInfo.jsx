87fc33ef8aa9fb69a9c9f3f045160f2440ad1a5d
Ratana <createbysenpai@gmail.com>
2024-04-28 14:43:15 +0200

    💄  Feature/dashboard-cards > Styling CardInfo

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -1,7 +1,32 @@
+import { PiggyBank, Wallet } from "lucide-react";
 import React from "react";
 
 function CardInfo({ budgetList }) {
-  return <div></div>;
+  return (
+    <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-5">
+      <div className="p-7 border rounded-lg flex items-center justify-between">
+        <div>
+          <h2 className="text-sm">Total Budget</h2>
+          <h2 className="font-bold text-2xl">1500€</h2>
+        </div>
+        <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+      </div>
+      <div className="p-7 border rounded-lg flex items-center justify-between">
+        <div>
+          <h2 className="text-sm">Total Spend</h2>
+          <h2 className="font-bold text-2xl">1500€</h2>
+        </div>
+        <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+      </div>
+      <div className="p-7 border rounded-lg flex items-center justify-between">
+        <div>
+          <h2 className="text-sm">Number of Budget</h2>
+          <h2 className="font-bold text-2xl">1500€</h2>
+        </div>
+        <Wallet className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+      </div>
+    </div>
+  );
 }
 
 export default CardInfo;
