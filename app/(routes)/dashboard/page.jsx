2a3fc4b90fc1a7889b6effbd2a8475add5f0826d
Ratana <createbysenpai@gmail.com>
2024-04-28 14:11:14 +0200

    ✨ Feature/dashboard-cards > Add username

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,8 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 import React from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const { user } = useUser();
+  return (
+    <div className="p-5">
+      <h2 className="font-bold text-3xl">Hi, {user?.fullName} ✌🏻</h2>
+      <p className="text-gray-500">
+        Here's what happening with your money, Let's manage your expenses
+      </p>
+    </div>
+  );
 }
 
 export default Dashboard;
