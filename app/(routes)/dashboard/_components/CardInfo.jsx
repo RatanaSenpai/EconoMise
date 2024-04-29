e7b745d5dea59bd3df0f6b80d450b0720e3730e1
Ratana <createbysenpai@gmail.com>
2024-04-29 09:00:48 +0200

    🚧 Feature/dashboard-cards > Show skeleton effect

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -4,9 +4,11 @@ import React, { useEffect, useState } from "react";
 function CardInfo({ budgetList }) {
   const [totalBudget, setTotalBudget] = useState(0);
   const [totalSpend, setTotalSpend] = useState(0);
+
   useEffect(() => {
     budgetList && CalculateCardInfo();
   }, [budgetList]);
+
   const CalculateCardInfo = () => {
     let totalBudget_ = 0;
     let totalSpend_ = 0;
@@ -19,28 +21,41 @@ function CardInfo({ budgetList }) {
   };
 
   return (
-    <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-5">
-      <div className="p-7 border rounded-lg flex items-center justify-between">
-        <div>
-          <h2 className="text-sm">Total Budget</h2>
-          <h2 className="font-bold text-2xl">{totalBudget}€ </h2>
+    <div>
+      {budgetList?.length > 0 ? (
+        <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-5">
+          <div className="p-7 border rounded-lg flex items-center justify-between">
+            <div>
+              <h2 className="text-sm">Total Budget</h2>
+              <h2 className="font-bold text-2xl">{totalBudget}€ </h2>
+            </div>
+            <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+          </div>
+          <div className="p-7 border rounded-lg flex items-center justify-between">
+            <div>
+              <h2 className="text-sm">Total Spend</h2>
+              <h2 className="font-bold text-2xl">{totalSpend}€</h2>
+            </div>
+            <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+          </div>
+          <div className="p-7 border rounded-lg flex items-center justify-between">
+            <div>
+              <h2 className="text-sm">Number of Budget</h2>
+              <h2 className="font-bold text-2xl">{budgetList?.length}</h2>
+            </div>
+            <Wallet className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
+          </div>
         </div>
-        <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
-      </div>
-      <div className="p-7 border rounded-lg flex items-center justify-between">
-        <div>
-          <h2 className="text-sm">Total Spend</h2>
-          <h2 className="font-bold text-2xl">{totalSpend}€</h2>
+      ) : (
+        <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-5">
+          {[1, 2, 3].map((item, index) => (
+            <div
+              key={index}
+              className="h-[110px] w-full bg-slate-200 animate-pulse rounded-lg"
+            ></div>
+          ))}
         </div>
-        <PiggyBank className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
-      </div>
-      <div className="p-7 border rounded-lg flex items-center justify-between">
-        <div>
-          <h2 className="text-sm">Number of Budget</h2>
-          <h2 className="font-bold text-2xl">{budgetList?.length}</h2>
-        </div>
-        <Wallet className="bg-primary p-3 h-12 w-12 rounded-full text-white" />
-      </div>
+      )}
     </div>
   );
 }
