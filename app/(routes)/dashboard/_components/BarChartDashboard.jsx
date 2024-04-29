b58306de7ea17581aac88796d23495f93bae07a6
Ratana <createbysenpai@gmail.com>
2024-04-29 11:13:39 +0200

    🚧  Budget-expenses-dashboard > Add expenses list on dashboard

--- app/(routes)/dashboard/_components/BarChartDashboard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashboard.jsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 function BarChartDashboard({ budgetList }) {
   return (
     <div className="border rounded-lg p-5">
       <h2 className="font-bold text-lg mb-5">Activity</h2>
-      <BarChart width={500} height={300} data={budgetList} margin={{ top: 7 }}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="totalSpend" stackId="a" fill="#8884d8" />
-        <Bar dataKey="amount" stackId="a" fill="#82ca9d" />
-      </BarChart>
+      <ResponsiveContainer width={"80%"} height={300}>
+        <BarChart data={budgetList} margin={{ top: 7 }}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="totalSpend" stackId="a" fill="#8884d8" />
+          <Bar dataKey="amount" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -8,11 +8,13 @@ import { Budgets, Expenses } from "@/utils/schema";
 import BudgetList from "./budgets/_components/BudgetList";
 import BarChartDashboard from "./_components/BarChartDashboard";
 import BudgetItem from "./budgets/_components/BudgetItem";
+import ExpenseListTable from "./expenses/_components/ExpenseListTable";
 
 function Dashboard() {
   const { user } = useUser();
 
   const [budgetList, setBudgetList] = useState([]);
+  const [expensesList, setExpensesList] = useState([]);
 
   useEffect(() => {
     user && getBudgetList();
@@ -29,8 +31,27 @@ function Dashboard() {
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
       .groupBy(Budgets.id)
       .orderBy(desc(Budgets.id));
+
     setBudgetList(result);
+    getAllExpenses();
   };
+
+  const getAllExpenses = async () => {
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id));
+
+    setExpensesList(result);
+  };
+
   return (
     <div className="p-5">
       <h2 className="font-bold text-3xl">Hi, {user?.fullName} ✌🏻</h2>
@@ -41,6 +62,10 @@ function Dashboard() {
       <div className="grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
         <div className="md:col-span-2">
           <BarChartDashboard budgetList={budgetList} />
+          <ExpenseListTable
+            expensesList={expensesList}
+            refreshData={() => getBudgetList()}
+          />
         </div>
         <div className="grid gap-5">
           <h2 className="font-bold text-lg">Latest Budgets</h2>
