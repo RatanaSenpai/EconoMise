01f24b07132fd59abbb8da9cd3b4019f970316d7
Ratana <createbysenpai@gmail.com>
2024-04-30 10:26:55 +0200

    🐛   Bugfix/general > Fix prevent non-numeric input in Expense Amount field

--- app/(routes)/dashboard/expenses/_components/AddExpense.jsx
+++ app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -7,28 +7,36 @@ import React, { useState } from "react";
 import { toast } from "sonner";
 
 function AddExpense({ budgetId, user, refreshData }) {
-  const [name, setName] = useState();
-  const [amount, setAmount] = useState();
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
 
   const addNewExpense = async () => {
     const result = await db
       .insert(Expenses)
       .values({
-        name: name,
+        name,
         amount: amount,
-        budgetId: budgetId,
+        budgetId,
         createdAt: moment().format("DD/MM/YYYY"),
       })
       .returning({ insertedId: Budgets.id });
 
-    setAmount("");
     setName("");
+    setAmount("");
 
     if (result) {
       refreshData();
       toast("New Expense Added!");
     }
   };
+
+  const handleAmountChange = (e) => {
+    const value = e.target.value;
+    if (!value || value.match(/^\d*$/)) {
+      setAmount(value);
+    }
+  };
+
   return (
     <div className="border p-5 rounded-lg">
       <h2 className="font-bold text-lg">Add Expenses</h2>
@@ -47,15 +55,20 @@ function AddExpense({ budgetId, user, refreshData }) {
         <Input
           value={amount}
           placeholder="e.g. 500€"
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={handleAmountChange}
         />
       </div>
       <Button
-        disabled={!(name && amount)}
-        onClick={() => addNewExpense()}
+        disabled={
+          !name.trim() ||
+          !amount.trim() ||
+          isNaN(parseFloat(amount)) ||
+          parseFloat(amount) <= 0
+        }
+        onClick={addNewExpense}
         className="mt-4 w-full"
       >
-        New Expenses
+        Add Expense
       </Button>
     </div>
   );
